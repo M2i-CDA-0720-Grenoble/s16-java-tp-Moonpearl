8b19b9d5645bb5264c763592133f1d342f2e7c8e
Romain Tello <romain.tello@gmail.com>
2020-11-27 10:33:04 +0100

    Solve step 1 - Add user input validation

--- src/App.java
+++ src/App.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+import java.util.regex.*;
+
 public class App
 {
     // Cette méthode est appelée automatiquement au lancement de l'application
@@ -5,7 +8,27 @@ public class App
         // Efface la console
         System.out.print("\033[H\033[2J");   
         System.out.flush();
-        
-        System.out.print("Hello, world!");
+
+        // Initialise un objet permettant de traiter la saisie de l'utilisateur        
+        Scanner scanner = new Scanner(System.in);
+
+        String userInput = null;
+        // Tant que la saisie de l'utilisateur n'est pas vide
+        while (!"".equals(userInput)) {
+            // Récupère une saisie de l'utilisateur
+            // en enlevant les espaces avant et après, et en convertissant en majuscules
+            userInput = scanner.nextLine().trim().toUpperCase();
+
+            // Vérifie si la saisie de l'utilisateur correspond au schéma attendu
+            // (exactement 4 caractères parmi R, V, B, J, C et M, sans rien avant ni rien après)
+            System.out.println(
+                Pattern.matches("^[RVBJCM]{4}$", userInput)
+            );
+        }
+
+        // Nettoie l'objet permettant de traiter la saisie de l'utilisateur
+        scanner.close();
+
+        System.out.print("Goodbye Marylou!");
     }
 }
