d69b16debab4d52b21e125cb0171d279a81e685f
Romain Tello <romain.tello@gmail.com>
2020-11-27 11:12:18 +0100

    Add correct colors count

--- src/App.java
+++ src/App.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 import java.util.regex.*;
 
+import Utils.ConsoleColor;
+
 public class App
 {
     // Cette méthode est appelée automatiquement au lancement de l'application
@@ -12,6 +14,10 @@ public class App
         // Initialise un objet permettant de traiter la saisie de l'utilisateur        
         Scanner scanner = new Scanner(System.in);
 
+        // Définit la solution du jeu
+        String solution = "MMCR";
+        char[] solutionArray = solution.toCharArray();
+
         String userInput = null;
         // Tant que la saisie de l'utilisateur n'est pas vide
         while (!"".equals(userInput)) {
@@ -21,14 +27,26 @@ public class App
 
             // Vérifie si la saisie de l'utilisateur correspond au schéma attendu
             // (exactement 4 caractères parmi R, V, B, J, C et M, sans rien avant ni rien après)
-            System.out.println(
-                Pattern.matches("^[RVBJCM]{4}$", userInput)
-            );
+            if (Pattern.matches("^[RVBJCM]{4}$", userInput)) {
+                char[] userInputArray = userInput.toCharArray();
+                
+                // Compte le nombre de couleurs bien placées dans la saisie de l'utilisateur
+                int correct = 0;
+                for (int i = 0; i < 4; i += 1) {
+                    if (userInputArray[i] == solutionArray[i]) {
+                        correct += 1;
+                    }
+                }
+                System.out.println("Vous avez " + correct + " couleurs bien placées.");
+
+            } else {
+                System.out.println(ConsoleColor.YELLOW + "Ceci n'est pas une combinaison valide!" + ConsoleColor.RESET);
+            }
         }
 
         // Nettoie l'objet permettant de traiter la saisie de l'utilisateur
         scanner.close();
 
-        System.out.print("Goodbye Marylou!");
+        System.out.print("Goodbye!");
     }
 }
